fa102d69b1ee767cf1c8ebb7b391fef59fbc98d1
Guilherme Araújo <guilhermearaujofsa2007@gmail.com>
2026-03-04 09:38:39 -0300

    Project created

---
+++ src/main/java/com/guilherme/movierating/MovieRatingApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.movierating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRatingApplication.class, args);
+    }
+
+}
