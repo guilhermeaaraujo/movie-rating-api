920db31f837840cfb1b18a6d0d6441958f330145
Guilherme Araújo <guilhermearaujofsa2007@gmail.com>
2026-03-09 08:53:30 -0300

    Swagger configuration + Documentation for AuthController

---
+++ src/main/java/com/guilherme/movierating/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.guilherme.movierating.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Movie Rating API")
+                        .version("1.0")
+                        .description("API Rest para usuários compartilharem críticas de filmes"));
+    }
+}
--- src/main/java/com/guilherme/movierating/controllers/AuthController.java
+++ src/main/java/com/guilherme/movierating/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.guilherme.movierating.controllers;
 
+import com.guilherme.movierating.config.SecurityConfig;
 import com.guilherme.movierating.config.TokenService;
 import com.guilherme.movierating.model.dto.request.LoginRequest;
 import com.guilherme.movierating.model.dto.request.RegisterRequest;
@@ -8,6 +9,10 @@ import com.guilherme.movierating.model.dto.response.UserDTO;
 import com.guilherme.movierating.model.entities.User;
 import com.guilherme.movierating.model.enums.UserRole;
 import com.guilherme.movierating.repositories.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "authentication", description = "Controlador para autenticação e registro de usuários")
+@SecurityRequirement(name = SecurityConfig.SECURITY)
 public class AuthController {
 
     @Autowired
@@ -36,6 +43,10 @@ public class AuthController {
     private TokenService tokenService;
 
     @PostMapping("/login")
+    @Operation(summary = "Logar usuário", description = "Metódo de login")
+    @ApiResponse(responseCode = "200", description = "Usuário logado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Email ou senha incorretos")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
         UsernamePasswordAuthenticationToken usernamepassword = new UsernamePasswordAuthenticationToken(request.email(), request.password());
         var authentication = authenticationManager.authenticate(usernamepassword);
@@ -45,6 +56,10 @@ public class AuthController {
         return ResponseEntity.ok(new LoginResponse(token));
     }
 
+    @Operation(summary = "Registrar usuário", description = "Registra um novo usuário no banco de dados")
+    @ApiResponse(responseCode = "201", description = "Usuário registrado com sucesso")
+    @ApiResponse(responseCode = "403", description = "Email ou senha incorretos")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     @PostMapping("/register")
     public ResponseEntity<UserDTO> register(@RequestBody RegisterRequest request) {
         if (repository.findByEmail(request.email()).isPresent()) return ResponseEntity.badRequest().build();
